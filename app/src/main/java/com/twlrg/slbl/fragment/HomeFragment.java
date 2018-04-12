540a2857b67577ba2be7316ab28390ecf76ebab9
wangxy2013 <wangxianyun1@163.com>
2018-04-12 17:12:21 +0800

    commit

---
+++ app/src/main/java/com/twlrg/slbl/fragment/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.twlrg.slbl.fragment;
+
+/**
+ * 作者：王先云 on 2018/4/12 16:50
+ * 邮箱：wangxianyun1@163.com
+ * 描述：首页
+ */
+public class HomeFragment extends BaseFragment
+{
+    @Override
+    protected void initData()
+    {
+
+    }
+
+    @Override
+    protected void initViews()
+    {
+
+    }
+
+    @Override
+    protected void initEvent()
+    {
+
+    }
+
+    @Override
+    protected void initViewData()
+    {
+
+    }
+}
