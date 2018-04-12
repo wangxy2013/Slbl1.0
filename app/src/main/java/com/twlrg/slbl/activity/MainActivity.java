132c29bea8322d301803f0338178776a4ad7cd01
wangxy2013 <wangxianyun1@163.com>
2018-04-12 21:05:47 +0800

    commit

--- app/src/main/java/com/twlrg/slbl/activity/MainActivity.java
+++ app/src/main/java/com/twlrg/slbl/activity/MainActivity.java
@@ -1,17 +1,89 @@
 package com.twlrg.slbl.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.twlrg.slbl.R;
+import com.twlrg.slbl.fragment.HomeFragment;
+import com.twlrg.slbl.fragment.MessageFragment;
+import com.twlrg.slbl.fragment.OrderFragment;
+import com.twlrg.slbl.fragment.UserCenterFragment;
 
-public class MainActivity extends AppCompatActivity
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity
 {
 
+
+    @BindView(android.R.id.tabhost)
+    FragmentTabHost fragmentTabHost;
+
+
+    private String texts[]         = {"首页", "消息", "订单", "我的"};
+    private int    imageButton[]   = {
+            R.drawable.ic_home_selector, R.drawable.ic_home_selector,
+            R.drawable.ic_home_selector, R.drawable.ic_home_selector};
+    private Class  fragmentArray[] = {HomeFragment.class, MessageFragment.class, OrderFragment.class, UserCenterFragment.class};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void initData()
     {
-        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState)
+    {
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initEvent()
+    {
+
+    }
+
+    @Override
+    protected void initViewData()
+    {
+        fragmentTabHost.setup(this, getSupportFragmentManager(),
+                R.id.main_layout);
+
+        for (int i = 0; i < texts.length; i++)
+        {
+            TabHost.TabSpec spec = fragmentTabHost.newTabSpec(texts[i]).setIndicator(getView(i));
+
+            fragmentTabHost.addTab(spec, fragmentArray[i], null);
+
+            //设置背景(必须在addTab之后，由于需要子节点（底部菜单按钮）否则会出现空指针异常)
+            // fragmentTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.drawable.main_tab_selector);
+        }
+        fragmentTabHost.getTabWidget().setDividerDrawable(R.color.transparent);
+    }
+
+    private View getView(int i)
+    {
+        //取得布局实例
+        View view = View.inflate(MainActivity.this, R.layout.tabcontent, null);
+        //取得布局对象
+        ImageView imageView = (ImageView) view.findViewById(R.id.image);
+        TextView textView = (TextView) view.findViewById(R.id.text);
+
+        //设置图标
+        imageView.setImageResource(imageButton[i]);
+        //设置标题
+        textView.setText(texts[i]);
+        return view;
+    }
+
+    public void changeTab(int index)
+    {
+        fragmentTabHost.setCurrentTab(index);
+
     }
 }
